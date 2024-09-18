02dd2e94c59fa5e727b8a88e662a95f7b691d059
Shantanu Aditya <117399781+AweShantanu@users.noreply.github.com>
2024-09-19 00:00:18 +0530

    Add files via upload

---
+++ build/precache-manifest.109e45e39db78d94a65ac861fbee9705.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "605126f97021ed2b38074f9e3947538e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7cbeedc400008955314",
+    "url": "/static/css/main.ca87938a.chunk.css"
+  },
+  {
+    "revision": "9cbe13e10c9f113485c1",
+    "url": "/static/js/2.e96df388.chunk.js"
+  },
+  {
+    "revision": "f7cbeedc400008955314",
+    "url": "/static/js/main.3396c6ad.chunk.js"
+  },
+  {
+    "revision": "f1b768174fa530eb5144",
+    "url": "/static/js/runtime-main.39dd3371.js"
+  }
+]);
